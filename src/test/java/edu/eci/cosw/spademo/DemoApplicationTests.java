6b74c1ccf8c702645ed6dfbbbb1c750fa7bc1c41
pipegomez <juan.gomez-r@hotmail.com>
2016-02-24 21:55:20 -0300

    Creando el git

--- src/test/java/edu/eci/cosw/spademo/DemoApplicationTests.java
+++ src/test/java/edu/eci/cosw/spademo/DemoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplication.class)
-@WebAppConfiguration
+//@WebAppConfiguration
 public class DemoApplicationTests {
 
 	@Test
