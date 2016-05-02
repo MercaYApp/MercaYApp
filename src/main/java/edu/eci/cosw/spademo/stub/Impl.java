02ceffa104e2547fc4d3105a0b8e9b61c73865ca
pipegomez <juan.gomez-r@hotmail.com>
2016-05-01 22:35:33 -0500

    Corregir post invoice

--- src/main/java/edu/eci/cosw/spademo/stub/Impl.java
+++ src/main/java/edu/eci/cosw/spademo/stub/Impl.java
@@ -154,7 +154,25 @@ public class Impl implements IStub {
 
     @Override
     public void postInvoice(Invoice i) {
+        //Agregar factura a los productos
+        Set<Product> products = i.getProductses();
+        Product product;
+        Iterator<Product> iterator = products.iterator();
+        while (iterator.hasNext()) {
+            product = iterator.next();
+            Set<Invoice> setInvoices = productsR.findOne(product.getIdProductos()).getInvoiceses();
+            setInvoices.add(i);
+            product.setInvoiceses(setInvoices);
+            System.out.println("PRODUCTOS LENGTH: "+product.getInvoiceses().size());
+            productsR.save(product);
+        }
+        
+        
+        //Agregar productos a la factura
+        i.setProductses(products);
         invoicesR.save(i);
+        
+        
     }
 
     @Override
